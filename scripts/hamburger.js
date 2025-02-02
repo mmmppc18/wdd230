793554235969d34c089cdceb62789fc06a7eb97f
mmmppc18 <and20147@byui.edu>
2025-02-01 22:59:55 -0700

    trying to fix hamburger

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,12 +1,8 @@
+
+const mainnax = document.querySelector('.navigation');
 const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('show');
+	mainav.classList.toggle('show');
 	hamButton.classList.toggle('open');
-	if (navigation.style.left === "0px") {
-		navigation.style.left = "-250px";
-	} else {
-		navigation.style.left = "0px";
-	}
 });
