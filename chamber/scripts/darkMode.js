4c6572d41b24a2d9d01f694a4f32c1a5eb63f8f6
mmmppc18 <and20147@byui.edu>
2025-02-13 21:42:11 -0700

    trying to fix nav menu and hamburger

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -1,10 +1,14 @@
-const modeSwitch =
-    document.getElementById("switch");
 const main =
     document.querySelector("main");
 
+const modeSwitch =
+    document.querySelector("input[type=checkbox]");
 
-modeSwitch.addEventListener("click", () => {
-    main.classList.toggle(body.darkMode)
-
-})
+modeSwitch.addEventListener('change', function () {
+    if (this.checked) {
+        console.log("");
+        main.classList.toggle(main.darkMode);
+    } else {
+        console.log("");
+    }
+});
