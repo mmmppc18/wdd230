0afd397b4725cc235eaa8542b0c611de928d1707
mmmppc18 <and20147@byui.edu>
2025-02-13 22:38:55 -0700

    change order of elements

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -3,10 +3,10 @@ const modeSwitch = document.querySelector("input[type=checkbox]");
 
 modeSwitch.addEventListener('change', function () {
     if (this.checked) {
-        main.classList.add("darkMode"); 
-        document.body.classList.add("darkMode"); 
+        main.classList.add("darkMode");
+        document.body.classList.add("darkMode");
     } else {
-        main.classList.remove("darkMode"); 
+        main.classList.remove("darkMode");
         document.body.classList.remove("darkMode");
     }
 });
