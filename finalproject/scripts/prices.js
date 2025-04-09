afc28d520f2d07d95d93ffef91135a390478dcb4
mmmppc18 <and20147@byui.edu>
2025-04-08 23:16:56 -0600

    building json and list of types

---
+++ finalproject/scripts/prices.js
@@ -0,0 +1,43 @@
+const url = 'C:\Users\mmmpp\Documents\01 Winter 2022\Web frontend\wdd230\finalproject\data\prices.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+
+}
+
+getProphetData(url);
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section')
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let birthplace = document.createElement('article');
+        let birthdate = document.createElement('article');
+
+
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        birthdate.textContent = `Date of Birth : ${prophet.birthdate}`;
+
+        birthplace.textContent = `Place of Birth : ${prophet.birthplace}`;
+
+        card.appendChild(fullName);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+
+    });
+}
