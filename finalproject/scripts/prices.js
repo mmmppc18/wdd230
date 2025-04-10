0ce1b7c6e8ba2a5df10eb8fdf96b10d77c293d7a
mmmppc18 <and20147@byui.edu>
2025-04-09 22:31:33 -0600

    fixing footers

--- finalproject/scripts/prices.js
+++ finalproject/scripts/prices.js
@@ -1,43 +1,19 @@
-const url = 'C:\Users\mmmpp\Documents\01 Winter 2022\Web frontend\wdd230\finalproject\data\prices.json';
-const cards = document.querySelector('#cards');
-
-async function getProphetData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    //console.table(data.prophets);
-    displayProphets(data.prophets);
-
-}
-
-getProphetData(url);
-
-const displayProphets = (prophets) => {
-    prophets.forEach((prophet) => {
-        let card = document.createElement('section')
-        let fullName = document.createElement('h2');
-        let portrait = document.createElement('img');
-        let birthplace = document.createElement('article');
-        let birthdate = document.createElement('article');
-
-
-
-        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
-        portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
-
-        birthdate.textContent = `Date of Birth : ${prophet.birthdate}`;
-
-        birthplace.textContent = `Place of Birth : ${prophet.birthplace}`;
-
-        card.appendChild(fullName);
-        card.appendChild(birthdate);
-        card.appendChild(birthplace);
-        card.appendChild(portrait);
-
-        cards.appendChild(card);
-
-    });
-}
+fetch('data/prices.json')
+      .then(response => response.json())
+      .then(data => {
+        const tableBody = document.querySelector('#rentalTable tbody');
+        data.prices.forEach(item => {
+          const row = `
+            <tr>
+              <td>${item.type}</td>
+              <td>${item.persons}</td>
+              <td>${item.rhd}</td>
+              <td>${item.rfd}</td>
+              <td>${item.whd}</td>
+              <td>${item.wfd}</td>
+            </tr>
+          `;
+          tableBody.insertAdjacentHTML('beforeend', row);
+        });
+      })
+      .catch(error => console.error('Error loading JSON:', error));
