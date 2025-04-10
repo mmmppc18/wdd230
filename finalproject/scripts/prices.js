de4a1b98e9c307af68581989bb3e7758a807649b
mmmppc18 <and20147@byui.edu>
2025-04-09 23:05:25 -0600

    stylizing home

--- finalproject/scripts/prices.js
+++ finalproject/scripts/prices.js
@@ -1,19 +1,19 @@
-fetch('data/prices.json')
-      .then(response => response.json())
-      .then(data => {
-        const tableBody = document.querySelector('#rentalTable tbody');
-        data.prices.forEach(item => {
-          const row = `
-            <tr>
-              <td>${item.type}</td>
-              <td>${item.persons}</td>
-              <td>${item.rhd}</td>
-              <td>${item.rfd}</td>
-              <td>${item.whd}</td>
-              <td>${item.wfd}</td>
-            </tr>
-          `;
-          tableBody.insertAdjacentHTML('beforeend', row);
-        });
-      })
-      .catch(error => console.error('Error loading JSON:', error));
+//fetch('data/prices.json')
+      //.then(response => response.json())
+      //.then(data => {
+       // const tableBody = document.querySelector('#rentalTable tbody');
+        //data.prices.forEach(item => {
+          //const row = `
+            //<tr>
+              //<td>${item.type}</td>
+             // <td>${item.persons}</td>
+             // <td>${item.rhd}</td>
+            //  <td>${item.rfd}</td>
+            //  <td>${item.whd}</td>
+          //    <td>${item.wfd}</td>
+          //  </tr>
+         // `;
+        //  tableBody.insertAdjacentHTML('beforeend', row);
+    //    });
+    //  })
+    //  .catch(error => console.error('Error loading JSON:', error));/
--- finalproject/scripts/weather.js
+++ finalproject/scripts/weather.js
@@ -1,10 +1,7 @@
 const currentTemp = document.querySelector('#current-temp');
 const currentHumidity = document.querySelector('#current-humidity');
-const forecastTemp = document.querySelector('#forecast-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const weatherDescription = document.querySelector('#weather-description');
-const highTempMessage = document.querySelector('#high-temp-message');
-const highTempDisplay = document.querySelector('#temp-max');
 
 
 async function apiFetch(url) {
@@ -34,7 +31,6 @@ const currentUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${20.42}
 const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${20.42}&lon=${-86.92}&appid=${`4d28a8b5d356758d518dc67e8c28e7c5`}&units=imperial`;
 
 apiFetch(currentUrl);
-apiFetch(forecastUrl);
 
 function displayCurrentWeather(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
@@ -47,39 +43,4 @@ function displayCurrentWeather(data) {
 
     weatherDescription.innerHTML = data.weather[0].description;
 
-    const highTemp = data.main.temp_max;
-    highTempMessage.innerHTML = `${highTemp}&deg;F`;
-}
-
-function displayForecast(data) {
-    const tomorrowForecast = getTomorrowForecast(data.list);
-    if (tomorrowForecast) {
-        forecastTemp.innerHTML = `${tomorrowForecast.main.temp}&deg;F`;
-    } else {
-        console.error('Forecast for 3:00 PM not found')
-    }
-}
-
-function getTomorrowForecast(forecastList) {
-    const tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    tomorrow.setHours(15, 0, 0, 0);
-
-    for (let forecast of forecastList) {
-        const forecastDate = new Date(forecast.dt * 1000);
-
-
-        const localTime = new Date(forecastDate.getTime() - (forecastDate.getTimezoneOffset() * 60000));
-
-
-        if (localTime.getDate() === tomorrow.getDate() && localTime.getHours() === 15) {
-            return forecast;
-        }
-        return null;
-    }
-
-    function closeMessage() {
-        console.log('Closing message...');
-        highTempMessage.style.display = 'none';
-    }
 }
