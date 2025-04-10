49c15d3c34d4e4458ea796f5273308bfd7a80f0e
mmmppc18 <and20147@byui.edu>
2025-04-09 23:30:30 -0600

    trying to fix json

--- finalproject/scripts/prices.js
+++ finalproject/scripts/prices.js
@@ -1,19 +1,7 @@
-//fetch('data/prices.json')
-      //.then(response => response.json())
-      //.then(data => {
-       // const tableBody = document.querySelector('#rentalTable tbody');
-        //data.prices.forEach(item => {
-          //const row = `
-            //<tr>
-              //<td>${item.type}</td>
-             // <td>${item.persons}</td>
-             // <td>${item.rhd}</td>
-            //  <td>${item.rfd}</td>
-            //  <td>${item.whd}</td>
-          //    <td>${item.wfd}</td>
-          //  </tr>
-         // `;
-        //  tableBody.insertAdjacentHTML('beforeend', row);
-    //    });
-    //  })
-    //  .catch(error => console.error('Error loading JSON:', error));/
+function generateTable(data) {
+    const table = document.getElementById("rentalTable");
+    if (!data || data.length === 0) {
+        table.innerHTML = "No data to display.";
+        return;
+    }
+}
